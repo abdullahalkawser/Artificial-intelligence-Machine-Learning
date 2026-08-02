b902cecd4d37edafd97691f3e0e6b4e51d2c8a9c
Prottoy Bhattacharyya <prottoybhattacharyya@gmail.com>
2026-08-02 13:40:40 +0600

    Add examples for modifying multiple values in a NumPy array

--- Data scince/Numpy/Array Modification/Array Modification.py
+++ Data scince/Numpy/Array Modification/Array Modification.py
@@ -20,3 +20,19 @@ b[1, 1] = 6 #index (0,1) er value 10  diye relace koira holo Row 0, Column 1-এ
 # এখন 50 হয়েছে।
 
 print('2D array after modification:', b)
+
+
+
+# 2. Modify Multiple Values
+
+# একসাথে অনেকগুলো value পরিবর্তন করা যায়।
+
+
+
+c = np.array([1, 2, 3, 4, 5])
+
+c[1:4] = 10 #index 1 theke 3 porjonto value 10 diye relace koira holo
+
+c[1:4]= [10,20,30] #index 1 theke 3 porjonto value 10,20,30 diye relace koira holo
+
+print('array after modifying multiple values:', c)
