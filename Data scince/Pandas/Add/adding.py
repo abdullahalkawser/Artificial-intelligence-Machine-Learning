72f3941841baed3c04d5e09a7cdddcdd274386bb
abdullahalkawser <abdullahalkawser1@gmail.com>
2026-08-10 15:11:13 +0600

    Add: Create adding.py to demonstrate DataFrame column addition and insertion

---
+++ Data scince/Pandas/Add/adding.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+         'Age': [25, 30, 35],
+        'City': ['New York', 'Los Angeles', 'Chicago'],
+        "Salary": [50000, 60000, 70000]
+
+
+         }
+
+df = pd.DataFrame(data)
+
+print('Before adding new column:');
+print(df);
+
+
+# Adding a new column 'bonus' to the DataFrame
+
+df['bonus'] =df['Salary'] * 0.10
+
+print('After adding new column:');
+print(df);
+
+#Using the INSERT() method to add a new column to the DataFrame
+
+df.insert(2, 'Country', ['USA', 'Canada', 'Mexico'])
+
+
+# df.insert(loc, column, value, allow_duplicates=False
+
+
+
+print('After using INSERT() method:');
+
+
+
+df.insert(0,"ID",[1,2,3,])
+print(df);
+
