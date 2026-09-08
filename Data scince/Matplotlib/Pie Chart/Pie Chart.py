096a91130583eb6370e381c1b615cb9eb9581856
abdullah al kawser <abdullahalkawser1@gmail.com>
2026-09-08 12:10:32 +0600

    Add Pie Chart visualization for student transportation data

---
+++ Data scince/Matplotlib/Pie Chart/Pie Chart.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+
+
+# Student transport data
+transport = ["Bus", "Rickshaw", "Metro", "Walking"]
+students = [40, 25, 20, 15]
+
+plt.pie(students, labels=transport, colors=['blue', 'green', 'yellow', 'red'],autopct='%1.1f%%', startangle=90, shadow=True, explode=(0.1, 0, 0, 0), wedgeprops={'edgecolor': 'green'})
+
+plt.legend()
+plt.title("Transportation Used by 100 Students")
+plt.show()
