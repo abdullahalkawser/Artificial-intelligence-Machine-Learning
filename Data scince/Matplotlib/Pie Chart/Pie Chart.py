0cb8c60ffa0d2b40b5544346b615382d4293e6c7
abdullah al kawser <abdullahalkawser1@gmail.com>
2026-09-09 12:00:41 +0600

    Update title in Pie Chart visualization for clarity on daily commute

--- Data scince/Matplotlib/Pie Chart/Pie Chart.py
+++ Data scince/Matplotlib/Pie Chart/Pie Chart.py
@@ -9,5 +9,5 @@ students = [40, 25, 20, 15]
 plt.pie(students, labels=transport, colors=['blue', 'green', 'yellow', 'red'],autopct='%1.1f%%', startangle=90, shadow=True, explode=(0.1, 0, 0, 0), wedgeprops={'edgecolor': 'green'})
 
 plt.legend()
-plt.title("Transportation Used by 100 Students")
+plt.title("Transportation Used by 100 Students  for Daily Commute")
 plt.show()
